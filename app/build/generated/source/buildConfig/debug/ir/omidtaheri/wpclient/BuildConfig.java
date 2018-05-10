c46dd29d5c2dca7600a8609f459a400642bc1865
Omid Taheri <m.omidtaheri@yahoo.com>
2018-05-10 19:31:42 +0430

    commit 3

--- app/build/generated/source/buildConfig/debug/ir/omidtaheri/wpclient/BuildConfig.java
+++ app/build/generated/source/buildConfig/debug/ir/omidtaheri/wpclient/BuildConfig.java
@@ -11,5 +11,5 @@ public final class BuildConfig {
   public static final int VERSION_CODE = 1;
   public static final String VERSION_NAME = "1.0.0";
   // Fields from build type: debug
-  public static final String BASE_URL = "http://newshaa.ir/wp-json/wp/v2/";
+  public static final String BASE_URL = YOUR_SITE;
 }
